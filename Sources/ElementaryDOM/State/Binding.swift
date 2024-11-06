7c2f5eec3a45c07355c52e0d8d7eedc095ecceb0
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2024-11-06 11:43:59 +0100

    bit of clean up

--- Sources/ElementaryDOM/State/Binding.swift
+++ Sources/ElementaryDOM/State/Binding.swift
@@ -1,6 +1,7 @@
 @propertyWrapper
 @dynamicMemberLookup
 public struct Binding<V> {
+    // TODO: maybe optimze direct state bindings by storing the state accessor instead of closures
     var get: () -> V
     var set: (V) -> Void
 
