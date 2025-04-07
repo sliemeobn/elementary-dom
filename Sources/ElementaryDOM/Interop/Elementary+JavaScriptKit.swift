debc2a0965be90759b022032958d6feab986a4de
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2025-04-07 15:13:57 +0200

    lifecycle fixes and less chatter in reconciler

--- Sources/ElementaryDOM/Interop/Elementary+JavaScriptKit.swift
+++ Sources/ElementaryDOM/Interop/Elementary+JavaScriptKit.swift
@@ -35,7 +35,9 @@ final class JSKitDOMInteractor: DOMInteracting {
         document.createElement(element.jsValue).object!
     }
 
-    func patchElementAttributes(_ node: Node, with attributes: _AttributeStorage, replacing: _AttributeStorage) {
+    func patchElementAttributes(
+        _ node: Node, with attributes: _AttributeStorage, replacing: _AttributeStorage
+    ) {
         // fast pass
         guard attributes != .none || replacing != .none else { return }
 
@@ -45,22 +47,27 @@ final class JSKitDOMInteractor: DOMInteracting {
             if let previousValue = previousIndex {
                 let oldValue = previous.remove(at: previousValue)
                 if !oldValue.value.utf8Equals(attribute.value) {
-                    print("updating attribute \(attribute.name) from \(oldValue.value ?? "") to \(attribute.value ?? "")")
+                    _printDebug(
+                        "updating attribute \(attribute.name) from \(oldValue.value ?? "") to \(attribute.value ?? "")"
+                    )
                     _ = node.setAttribute!(attribute.name.jsValue, attribute.value.jsValue)
                 }
             } else {
-                print("setting attribute \(attribute.name) to \(attribute.value ?? "")")
+                _printDebug("setting attribute \(attribute.name) to \(attribute.value ?? "")")
                 _ = node.setAttribute!(attribute.name.jsValue, attribute.value.jsValue)
             }
         }
 
         for attribute in previous {
-            print("removing attribute \(attribute.name)")
+            _printDebug("removing attribute \(attribute.name)")
             _ = node.removeAttribute!(attribute.name)
         }
     }
 
-    func patchEventListeners(_ node: Node, with listers: _DomEventListenerStorage, replacing: _DomEventListenerStorage, sink: @autoclosure () -> EventSink) {
+    func patchEventListeners(
+        _ node: Node, with listers: _DomEventListenerStorage, replacing: _DomEventListenerStorage,
+        sink: @autoclosure () -> EventSink
+    ) {
         guard !(listers.listeners.isEmpty && replacing.listeners.isEmpty) else { return }
 
         var previous = replacing.listeners.map { $0.event }
@@ -70,13 +77,13 @@ final class JSKitDOMInteractor: DOMInteracting {
             if let previousIndex {
                 previous.remove(at: previousIndex)
             } else {
-                print("adding listener \(event)")
+                _printDebug("adding listener \(event)")
                 _ = node.addEventListener!(event.jsValue, sink().jsValue)
             }
         }
 
         for event in previous {
-            print("removing listener \(event)")
+            _printDebug("removing listener \(event)")
             _ = node.removeEventListener!(event.jsValue, sink().jsValue)
         }
     }
@@ -87,7 +94,7 @@ final class JSKitDOMInteractor: DOMInteracting {
     }
 
     func replaceChildren(_ children: [Node], in parent: Node) {
-        print("setting \(children.count) children in \(parent)")
+        _printDebug("setting \(children.count) children in \(parent)")
         let function = parent.replaceChildren.function!
         function.callAsFunction(
             this: parent,
@@ -96,9 +103,17 @@ final class JSKitDOMInteractor: DOMInteracting {
     }
 
     func requestAnimationFrame(_ callback: @escaping (Double) -> Void) {
-        _ = JSObject.global.requestAnimationFrame!(JSClosure { args in
-            callback(args[0].number!)
-            return .undefined
-        }.jsValue)
+        _ = JSObject.global.requestAnimationFrame!(
+            JSClosure { args in
+                callback(args[0].number!)
+                return .undefined
+            }.jsValue)
+    }
+
+    private func _printDebug(_ message: @autoclosure () -> String) {
+        // TODO: make this conditional somehow
+        if false {
+            print(message())
+        }
     }
 }
