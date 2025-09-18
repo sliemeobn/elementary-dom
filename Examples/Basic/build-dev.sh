0eb0c9c4ad6084d3d2de4146967f87bb688a6312
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2025-09-18 16:42:37 +0200

    switched to awk and release images

--- Examples/Basic/build-dev.sh
+++ Examples/Basic/build-dev.sh
@@ -2,6 +2,8 @@ OUTDIR=Public/lib/example
 set -ex
 
 swift package \
-  --swift-sdk "$(swiftc -print-target-info | jq -r '.swiftCompilerTag')_wasm" \
+  --swift-sdk "$(swiftc -print-target-info | awk -F'"' '/swiftCompilerTag/ {print $4}')_wasm" \
   --allow-writing-to-package-directory \
   js -c debug --output $OUTDIR --use-cdn
+
+  
