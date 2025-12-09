4c5a39077d141d3a80cc0179f86cff30db1a854b
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2025-12-09 13:24:04 +0100

    added tracelog switch

--- Examples/Basic/Package.swift
+++ Examples/Basic/Package.swift
@@ -1,11 +1,13 @@
 // swift-tools-version:6.2
 import PackageDescription
 
+let traceLogs = Context.environment["TRACE_LOGS"].flatMap { Bool($0) } ?? false
+
 let package = Package(
     name: "BasicExample",
     platforms: [.macOS(.v15)],
     dependencies: [
-        .package(name: "elementary-ui", path: "../../"),
+        .package(name: "elementary-ui", path: "../../", traits: traceLogs ? ["TraceLogs"] : []),
         .package(url: "https://github.com/swiftwasm/JavaScriptKit.git", from: "0.36.0"),
     ],
     targets: [
